ef486497414a8911565a1fd2641df16a748cd0f0
JadesonBruno <jadesonbruno.a@outlook.com>
2025-03-16 15:20:46 -0300

    :sparkles: feat: Adding new function to export files in csv or parquet

--- etl.py
+++ etl.py
@@ -8,8 +8,11 @@ from typing import List
 import pandas as pd
 
 
-# function to extract and cosolidate json file
 def extract_and_consolidate_json_files(path: Path) -> pd.DataFrame:
+    """
+    Function that extracts data from multiple json files and consolidates
+    them into a pandas dataframe.
+    """
     json_files: List[Path] = glob.glob(os.path.join(path, "*.json"))
 
     dataframes_list: List[pd.DataFrame] = [
@@ -21,7 +24,23 @@ def extract_and_consolidate_json_files(path: Path) -> pd.DataFrame:
 
 
 def calculate_kpi_of_total_sales(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Function that creates a new calculated column for total sales.
+    """
     new_dataframe: pd.DataFrame = dataframe.copy()
     new_dataframe["total_sales"] = new_dataframe["Quantidade"] * new_dataframe["Venda"]
 
     return new_dataframe
+
+
+def load_data(sales_dataframe: pd.DataFrame, exit_format: List[str]) -> None:
+    """
+    Function that exports data to a csv file or parquet file.
+    """
+    if "csv" in exit_format:
+        sales_dataframe.to_csv(path_or_buf="output/data.csv", index=False)
+        print("Exporting to csv.")
+
+    if "parquet" in exit_format:
+        sales_dataframe.to_parquet(path="output/data.parquet", index=False)
+        print("Exporting to parquet.")
